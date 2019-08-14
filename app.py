c3104b1650d101e8d9516fc4b3d5c450f8e530d3
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-13 21:57:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,25 @@ from gutenberg.cleanup import strip_headers
 import random
 import os 
 from flask import send_from_directory
-
+import boto3
+from flask import Flask, request
+from flask import Flask, Response
+from boto3 import client
+import botocore
+from boto.s3.key import Key
 
 
 
 app = Flask(__name__, static_url_path='/static')
 
+
+def get_client():
+    return client(
+        's3',
+        'us-east-1',
+        aws_access_key_id='AKIAT2WU5IJC57E53KM6',
+        aws_secret_access_key='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'
+    )
 
 
 
@@ -18,9 +31,6 @@ app = Flask(__name__, static_url_path='/static')
 def welcome():
     return 'Welcome to the server'
 
-@app.route('/books')
-def books():
-    return render_template("main.html")
 
 @app.route('/favicon.ico')
 def favicon():
@@ -28,13 +38,19 @@ def favicon():
        
 @app.route('/roulette', methods=['POST', 'GET'])
 def roulette():
+    s3 = get_client()
     f = open("GutProject.txt", "w")
     for x in range(1):
         y = (random.randint(0, 59000))
         text = strip_headers(load_etext(y)).strip()
-        f.write(text)
+        book = f.write(text)
+        client = boto3.client('s3')
+        response = client.put_object( 
+           Bucket='book-roulette',
+           Body='book',
+           Key='files/GutProject.txt')
         return render_template("main.html")
-       
+        
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
