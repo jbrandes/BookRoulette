05da5cea1156467f94448ce9e0d7fd569fe55645
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-14 11:01:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,14 @@ from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
 import random
 import os 
-from flask import send_from_directory
-import boto3
 from flask import Flask, request
 from flask import Flask, Response
-from boto3 import client
-import botocore
-from boto.s3.key import Key
+from flask import send_file, send_from_directory, safe_join, abort
 
 
 
 app = Flask(__name__, static_url_path='/static')
 
-
-def get_client():
-    return client(
-        's3',
-        'us-east-1',
-        aws_access_key_id='XXXXXX',
-        aws_secret_access_key='XXXXXXX'
-    )
-
-s3 = get_client()
 
 
 @app.route('/')
@@ -38,23 +24,15 @@ def favicon():
        
 @app.route('/roulette', methods=['POST', 'GET'])
 def roulette():
-    get_client()
-    file = s3.get_object(Bucket='book-roulette', Key='files/GutProject.txt')
     f = open("GutProject.txt", "w")
     for x in range(1):
         y = (random.randint(0, 59000))
         text = strip_headers(load_etext(y)).strip()
-        book = f.write(text)
-        client = boto3.client('s3')
-        response = client.put_object( 
-           Bucket='book-roulette',
-           Body= book,
-           Key='files/GutProject.txt')
-        return Response(
-                   file['Body'].read(),
-                   mimetype='text/plain',
-                   headers={"Content-Disposition": "attachment;filename=GutProject.txt"}
-    )
+        path = f.write(text)
+        return send_file(path, as_attachment=True)
+        
+    
+    
         
 
 if __name__ == '__main__':
