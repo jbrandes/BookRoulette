5921a1e00947c423cd8118cfddbba6e20eadfd44
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-16 16:13:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,22 +14,22 @@ app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
-def welcome():
-    return 'Welcome to the server'
+def home():
+    render_template('index.html', name=name)
 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
        
-@app.route('/roulette', methods=['POST', 'GET'])
+@app.route('/button', methods=['POST', 'GET'])
 def roulette():
     f = open("GutProject.txt", "w")
     for x in range(1):
         y = (random.randint(0, 59000))
         text = strip_headers(load_etext(y)).strip()
-        path = f.write(text)
-        return send_file(path, as_attachment=True)
+        f.write(text)
+        pass
         
     
     
