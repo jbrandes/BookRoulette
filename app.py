ffbb84d5cda72add59580688f1902248200f9652
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-17 22:13:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
 import random
-from random import *
 import os 
 from flask import Flask, request
 from flask import Flask, Response
@@ -27,21 +26,13 @@ def favicon():
        
 @app.route('/button', methods=['POST', 'GET'])
 def roulette():
-    f = open("GutProject.txt", "w")
-    for x in range(1):
-        y = (random.randint(0, 59000))
-        text = strip_headers(load_etext(y)).strip()
+    with open("GutProject.txt", "w") as f:
+        x = (random.randint(1, 590))
+        text = strip_headers(load_etext(x)).strip()
         f.write(text)
+        f.close()
     return send_file('GutProject.txt',
     mimetype='text/plain',
     attachment_filename='GutProject.txt',
     as_attachment=True)
-       
-
-        
     
-    
-        
-
-if __name__ == '__main__':
-    app.run(debug=True)
